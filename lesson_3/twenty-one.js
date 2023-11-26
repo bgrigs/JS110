2d85820d4a4fb86e6c143bb58ffa75e397d8da80
Bethany Grigsby <grigsby.bethany@gmail.com>
2023-11-26 13:42:56 -0800

    added player and dealer turns

--- lesson_3/twenty-one.js
+++ lesson_3/twenty-one.js
@@ -21,7 +21,10 @@
 */
 
 // console.log(`${deck[randomSuit][1][randomCard]} of ${deck[randomSuit][0]}`);
+const readline = require('readline-sync');
+const MAX_POINTS = 21;
 const CARDS_INITIAL_HAND = 2;
+const CARDS_HIT = 1;
 const WORTH_10 = ['J', 'Q', 'K'];
 
 playGame();
@@ -33,21 +36,85 @@ function playGame() {
   let deck = initializeDeck();
   deal(playerCards, dealerCards, deck);
 
-  let playerHandValue = countHandValue(playerCards);
-  let dealerHandValue = countHandValue(dealerCards);
+  let playerHandValue = () => countHandValue(playerCards);
+  let dealerHandValue = () => countHandValue(dealerCards);
 
-  displayAllCards(playerCards, playerHandValue);
-  displayLineBreak();
+  displayAllCards(playerCards, playerHandValue());
+  displayDealerCard(dealerCards, dealerHandValue());
 
-  displayDealerCard(dealerCards, dealerHandValue);
-  displayLineBreak();
+  while (true) {
+    if (!playerTurn(playerHandValue, playerCards, deck)) break;
+    if (playerLost(playerHandValue)) break;
+    if (playerHandValue === MAX_POINTS) {
+      console.log('Player wins');
+      break;
+    }
+  }
 
-  // dealRandomCard(playerCards, 1, deck);
-  // countHandValue(playerCards); // handValue isn't updating after 3rd card is drawn
-  // displayAllCards(playerCards, playerHandValue);
+  while (true) {
+    if (!dealerTurn(dealerHandValue, dealerCards, deck)) break;
+  }
 
 }
 
+function playerTurn(playerHandValue, playerCards, deck) {
+  while (true) {
+    if (playerLost(playerHandValue)) {
+      console.log('Player has busted. Dealer wins');
+      break;
+    }
+    if (playerHitOrStay() === 'hit') {
+      hit(playerCards, deck);
+      displayAllCards(playerCards, playerHandValue());
+    } else break;
+  }
+
+  return false;
+}
+
+function dealerTurn(dealerHandValue, dealerCards, deck) {
+  while (true) {
+    if (dealerHandValue() < 17) {
+      hit(dealerCards, deck);
+    }
+    if (dealerHandValue() > MAX_POINTS) {
+      console.log('dealer loses');
+      break;
+    }
+    if (dealerHandValue() >= 17) {
+      console.log(dealerCards);
+      console.log(`dealer hand value is ${dealerHandValue()}`);
+      break;
+    }
+  }
+
+  return false;
+}
+
+function playerLost(playerHandValue) {
+  return playerHandValue() > 21;
+}
+
+function playerHitOrStay() {
+  console.log(`Would you like to hit or stay? Press 'h' for hit and 's' for stay`);
+
+  while (true) {
+    let answer = readline.prompt().trim();
+    if (answer.toLowerCase() === 's' || answer.toLowerCase() === 'stay') {
+      answer = 'stay';
+      return answer;
+    } else if (answer.toLowerCase() === 'h' || answer.toLowerCase() === 'hit') {
+      answer = 'hit';
+      return answer;
+    } else {
+      console.log(`That is an invalid answer. Press 'h' for hit and 's' for stay`);
+    }
+  }
+}
+
+function hit(playerCards, deck) {
+  dealRandomCard(playerCards, CARDS_HIT, deck);
+}
 
 function countHandValue(hand) {
   // console.log(`cards in hand: ${hand.length}`);
@@ -93,6 +160,7 @@ function displayAllCards(hand, handValue) {
   console.log(`Player has: `);
   hand.forEach(card =>  console.log(`** ${card[1]} of ${card[0]}`));
   console.log(`** Hand value: ${handValue}`);
+  displayLineBreak();
 }
 
 function displayDealerCard(dealerCards, dealerHandValue) {
@@ -100,7 +168,7 @@ function displayDealerCard(dealerCards, dealerHandValue) {
   console.log(`Dealer has: `);
   console.log(`** ${card[1]} of ${card[0]}`);
   console.log(`** Hand value: ${dealerHandValue}`);
-
+  displayLineBreak();
 }
 
 function dealRandomCard(hand, cardsToDeal, deck) {
@@ -119,9 +187,6 @@ function getRandomCardIndex(randomSuit, deck) {
   let randomCardIndex = Math.floor(Math.random() * deck[randomSuit][1].length);
   return randomCardIndex;
 }
-
-
-// console.log(`${deck[randomSuit][1][randomCard]} of ${deck[randomSuit][0]}`);
 
 function displayLineBreak() {
   console.log('');
